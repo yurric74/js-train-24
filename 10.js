7573cc30010db1d71f5ad0c2954af18fb240663f
unknown <yurric@ukr.net>
2023-09-19 16:50:19 +0300

    js-train-24

--- 10.js
+++ 10.js
@@ -1,9 +1,29 @@
 console.log("Завдання: 10 ==============================");
 
 function task10() {
+  const promise1 = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Promise 1");
+    }, 500);
+  });
   // Створюємо проміс promise1, які вирішуються через 500мс, та повертають як успішне виконання рядок "Promise 1".
+  const promise2 = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Promise 2");
+    }, 200);
+  });
   // Створюємо проміс promise2, які вирішуються через 200мс, та повертають як успішне виконання рядок "Promise 2".
+  const promise3 = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Promise 3");
+    }, 300);
+  });
   // Створюємо проміс promise3, які вирішуються через 300мс, та повертають як успішне виконання рядок "Promise 3".
+  const promises = [promise1, promise2, promise3];
+
+  Promise.any(promises)
+    .then((data) => {console.log(data);})
+    .catch((error) => {console.error(error);});
   // Promise.any() приймає масив промісів які ми створили вище і вирішується, як тільки перший із цих промісів вирішується.
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
--- 9.js
+++ 9.js
@@ -1,10 +1,16 @@
 console.log("Завдання: 9 ==============================");
 
 function task9() {
+  function fetchWithError() {
+    const errorMessage = "Помилка при з'єднанні з сервером";
+    return Promise.reject(errorMessage);}
   // Створюємо функцію fetchWithError, яка симулює помилку при запиті до сервера.
   // Створюємо константу errorMessage в яку записуємо рядок "Помилка при з'єднанні з сервером"
   // Використовуємо Promise.reject(errorMessage) для створення вже відхиленого промісу.
+  fetchWithError()
+    .then((data) => { console.log(data);})
   // Викликаємо функцію fetchWithError
+  .catch((error) => { console.error("Помилка:", error);});
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
 }
